f47ae27b8d6ee114aad8a11a4c4292f5eb5bf3b3
Cristian Mihai Popa <p01c02w16@gmail.com>
2025-11-06 20:31:22 +0200

    Initial commit for deployment
    Signed-off-by: Cristian Mihai Popa <p01c02w16@gmail.com>

---
+++ frontend/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import requests
+
+st.title("Frontend Streamlit")
+
+backend_url = "https://cc2025-api-b2btduguh5cmdxbn.northeurope-01.azurewebsites.net/api/data"
+
+try:
+    response = requests.get(backend_url)
+    data = response.json()
+    st.success(f"Mesaj de la backend  : {data['message']}")
+except Exception as e:
+    st.error(f"Nu s-a   putut conecta la backend: {e}")
